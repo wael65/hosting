ed5f952386262c60acd7184818fc7cc7ace488a7
wael65 <waelebrahim65@gmail.com>
2022-02-21 16:15:18 +0200

    change in add from netlify in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React
+          Hello React from netlify
         </p>
         <a
           className="App-link"
